5a47a0f46870fadf4d5ca449140a97b93221cdee
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2022-01-30 12:25:17 +0700

    done item page

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const adminController = require("../controllers/adminController");
-const { upload } = require("../middlewares/multer");
+const { upload, uploadMultiple } = require("../middlewares/multer");
 
 router.get("/dashboard", adminController.viewDashboard);
 
@@ -16,7 +16,14 @@ router.post("/bank", upload, adminController.addBank);
 router.put("/bank", upload, adminController.updateBank);
 router.delete("/bank/:id", adminController.deleteBank);
 
+// endpoint item
 router.get("/item", adminController.viewItem);
+router.post("/item", uploadMultiple, adminController.addItem);
+router.get("/item/show-image/:id", adminController.showImageItem);
+router.get("/item/:id", adminController.showEditItem);
+router.put("/item/:id", uploadMultiple, adminController.updateItem);
+router.delete("/item/:id", adminController.deleteItem);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
